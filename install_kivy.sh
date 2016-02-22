cbc318c36f4b943f54d5e04a469febeaf8d8f768
Jan Kantert <jan-mpf@kantert.net>
2016-02-22 17:56:28 +0100

    more prerequisits for kivy

--- install_kivy.sh
+++ install_kivy.sh
@@ -5,3 +5,5 @@ add-apt-repository -y ppa:kivy-team/kivy
 apt-get update
 
 apt-get install -y python3-kivy
+
+apt-get install -y cython3 libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libav-tools libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
